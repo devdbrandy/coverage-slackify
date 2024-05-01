8de7c0c2e4c27bd912975db75e7c37f6fe4a4141
Harry Isidore <devdbrandy@gmail.com>
2024-05-01 15:32:22 +0200

    chore: refactor config sanitizer

--- src/cli-options.ts
+++ src/cli-options.ts
@@ -2,14 +2,15 @@ import {
   OptionsType,
   CoverageOptions,
   SlackOptions,
+  PackageJsonSchema,
 } from './types/config.type';
 
 export class CliOption {
-  private readonly config: OptionsType;
+  private readonly opts: OptionsType;
 
-  constructor(options: { name: string; coverageSlackify: OptionsType }) {
-    this.config = Object.assign(this.defaultOpts, {});
-    this.sanitizeConfig(options?.coverageSlackify, options?.name);
+  constructor(options: PackageJsonSchema) {
+    this.opts = Object.assign(this.defaultOpts, {});
+    this.sanitizeConfig(options);
   }
 
   private get defaultOpts(): OptionsType {
@@ -28,52 +29,46 @@ export class CliOption {
     };
   }
 
-  sanitizeConfig(coverageSlackify: OptionsType, packageName: string) {
-    this.config.coverage.rootDir =
-      coverageSlackify?.coverage?.rootDir ||
-      this.defaultOpts?.coverage?.rootDir;
-    this.config.coverage.threshold =
-      coverageSlackify?.coverage?.threshold ||
+  sanitizeConfig(packageConfig: PackageJsonSchema) {
+    this.opts.coverage.threshold =
+      packageConfig?.coverageSlackify?.threshold ||
       this.defaultOpts?.coverage?.threshold;
-    this.config.coverage.coverageFiles =
-      coverageSlackify?.coverage?.coverageFiles ||
+    this.opts.coverage.coverageFiles =
+      packageConfig?.coverageSlackify?.coverageFiles ||
       this.defaultOpts?.coverage?.coverageFiles;
-    this.config.coverage.coverageSummaryFile =
-      coverageSlackify?.coverage?.coverageSummaryFile ||
-      this.defaultOpts?.coverage?.coverageSummaryFile;
-    this.config.projectName =
-      coverageSlackify?.projectName ||
+    this.opts.projectName =
+      packageConfig?.coverageSlackify?.projectName ||
       this.defaultOpts?.projectName ||
-      packageName;
+      packageConfig?.name;
 
-    this.sanitizeThreshold(this.config.coverage.threshold);
+    this.sanitizeThreshold(this.opts.coverage.threshold);
 
-    if (this.config.coverage.coverageFiles.length === 0) {
+    if (this.opts.coverage.coverageFiles.length === 0) {
       throw new Error('Require at least one coverage istanbul file');
     }
   }
 
   private sanitizeThreshold(threshold: number) {
     if (threshold > 100) {
-      this.config.coverage.threshold = 100;
+      this.opts.coverage.threshold = 100;
     } else if (threshold < 0) {
-      this.config.coverage.threshold = 0;
+      this.opts.coverage.threshold = 0;
     }
   }
 
   get projectName(): string | undefined {
-    return this.config.projectName;
+    return this.opts.projectName;
   }
 
   get coverage(): CoverageOptions {
-    return this.config.coverage;
+    return this.opts.coverage;
   }
 
   get slack(): SlackOptions {
-    return this.config.slack;
+    return this.opts.slack;
   }
 
   get useTextNotify(): boolean {
-    return this.config.useTextNotify;
+    return this.opts.useTextNotify;
   }
 }
--- src/types/config.type.ts
+++ src/types/config.type.ts
@@ -1,9 +1,20 @@
-export interface OptionsType {
+export type PackageJsonSchema = {
+  name: string;
+  coverageSlackify: Config;
+};
+
+export type Config = {
+  projectName: string;
+  threshold: number;
+  coverageFiles: string[];
+};
+
+export type OptionsType = {
   projectName: string | undefined;
   coverage: CoverageOptions;
   slack: SlackOptions;
   useTextNotify: boolean;
-}
+};
 
 export type CoverageOptions = {
   rootDir: string;
